dd1a70dfce822dc915707493f0534c1cc51509fb
rameshpvr <rameshpvr97@gmail.com>
2020-07-05 00:41:23 +0530

    Updates

---
+++ precache-manifest.7193130e18ef876d853176a3d596cf90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c679f10843acc99ca81942ef8f3358cb",
+    "url": "/React_Todo/index.html"
+  },
+  {
+    "revision": "6d0330831f86e85edf8f",
+    "url": "/React_Todo/static/css/main.fec516df.chunk.css"
+  },
+  {
+    "revision": "971894d4c8b34762d2bf",
+    "url": "/React_Todo/static/js/2.617b2434.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Todo/static/js/2.617b2434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0330831f86e85edf8f",
+    "url": "/React_Todo/static/js/main.6569fea4.chunk.js"
+  },
+  {
+    "revision": "a9bd2661609b819462ab",
+    "url": "/React_Todo/static/js/runtime-main.dda64c68.js"
+  }
+]);
